defb003c5acd3a1b2fbd552c30cf0dd14d880487
sneakynade <sneakynade@gmail.com>
2015-10-29 06:02:04 -0400

    Update MovementSystem.cs

--- MovementSystem.cs
+++ MovementSystem.cs
@@ -15,7 +15,6 @@ public class MovementSystem : MonoBehaviour {
 	public AudioSource AudioObject;
 	public bool UseAudio;
 
-
 	//Setup Postition
 	public float CurrentX;
 	public float CurrentY;
@@ -37,7 +36,6 @@ public class MovementSystem : MonoBehaviour {
 	public float MoveX;
 	public float MoveY;
 	public float MoveZ;
-
 	public bool MoveXPositive;
 	public bool MoveYPositive;
 	public bool MoveZPositive;
@@ -63,128 +61,70 @@ public class MovementSystem : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	
-	
-	
 	{
-		CurrentMovementTime = System.DateTime.Now.Millisecond;
 	
-
-
-
-
+	 CurrentMovementTime = System.DateTime.Now.Millisecond;
 		//Check if ready to move
 
 		if (Move == 1) 
 		{
-
 			if (UseAudio == true) {AudioObject.enabled = true;}
-
-
 			if (CurrentMovementTime == MovementDifference) {} else
 
 			{
 				MovementDifference = CurrentMovementTime;
 				ValueTIme +=1;
 				//Move in the set directions
-
 				//X Direction
 				if (MoveX ==1f) 
 				{
 					if (MoveXPositive == true)
 					{CurrentX+= MoveSpeed; }
-					else
-					{
-						CurrentX-= MoveSpeed;
-					}
-				
+					else {CurrentX-= MoveSpeed;}
 				}
-
 				//Y Direction
 				if (MoveY ==1f) 
 				{
 					if (MoveYPositive == true)
 					{CurrentY+= MoveSpeed; }
-					else
-					{
-						CurrentY-= MoveSpeed;
-					}
-					
+					else{CurrentY-= MoveSpeed;}
 				}
-
-
 				//Z Direction
 
 				if (MoveZ ==1f) 
 				{
 					if (MoveZPositive == true)
 					{CurrentZ+= MoveSpeed; }
-					else
-					{
-						CurrentZ-= MoveSpeed;
-					}
-					
+					else{CurrentZ-= MoveSpeed;}
 				}
 
 				//Move the object
-
 				ObjectToMove.transform.position = new Vector3(CurrentX,CurrentY,CurrentZ);
 				if (ValueTIme == MovementTime) { 
 					Move=0; 
 					if (UseAudio == true) {AudioObject.enabled = false;}
 					ValueTIme = 0f;
-
 					if (EnableSecondTime = true) {OtherTimer.GetComponent<MovementSystem>().Move = 1; }
-
 				}
-
 			}
-
-
 		}
-
-
-
-
 	}
 
 //ButtonVariables
 
 	public void ButtonStart()
-
-	{
-		Move = 1;
-	}
+        {Move = 1;}
 
 	public void PositiveXButton()
-
-	{
-		MoveXPositive = true;
-	}
+	{MoveXPositive = true;}
 	public void PositiveYButton()
-		
-	{
-		MoveYPositive = true;
-	}
+	{MoveYPositive = true;}
 	public void PositiveZButton()
-		
-	{
-		MoveZPositive = true;
-	}
+	{MoveZPositive = true;}
 	public void NegZButton()
-		
-	{
-		MoveZPositive = false;
-	}
+	{MoveZPositive = false;}
 	public void NegXButton()
-		
-	{
-		MoveXPositive = false;
-	}
+	{MoveXPositive = false;}
 	public void NegYButton()
-		
-	{
-		MoveYPositive = false;
-	}
-
-
+	{MoveYPositive = false;}
 }
